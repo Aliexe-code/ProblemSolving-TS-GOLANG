de722cbdb8247b4cdc01b9447276ab42a931dc75
Ali Mohammed <terminalexe1@gmail.com>
2025-02-13 00:48:57 +0200

    Update fightOverCandies.ts

--- Problems/fightOverCandies.ts
+++ Problems/fightOverCandies.ts
@@ -1,5 +1,4 @@
-//https://atcoder.jp/contests/abc047/tasks/abc047_a
- export function fightOverCandies(a:number,b:number,c:number):string{
+export function fightOverCandies(a:number,b:number,c:number):string{
     if(a<=0 || b<=0 || c<=0) return 'No';
     if(a+b===c || a+c===b || b+c===a) return "YES"
     return "NO"
