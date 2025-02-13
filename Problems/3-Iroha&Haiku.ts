89d2b50eef7ead2294d8b0f71e4b51285f6c4a84
ali <terminalexe1@gmail.com>
2025-02-14 00:01:19 +0200

    fixed files

--- Problems/3-Iroha&Haiku.ts
+++ Problems/3-Iroha&Haiku.ts
@@ -1,8 +1,5 @@
-//https://atcoder.jp/contests/abc042/tasks/abc042_a
-export function irohaHaiku(A:number,B:number,C:number):string{
-    const sortedPattern = [5,5,7];
-    const input = [A,B,C].sort((x,y)=>x-y)
-    return input.toString()===sortedPattern.toString()? "YES" : "NO"
-
+export function irohaHaiku(A: number, B: number, C: number): string {
+  const sortedPattern = [5, 5, 7];
+  const input = [A, B, C].sort((x, y) => x - y);
+  return input.toString() === sortedPattern.toString() ? 'YES' : 'NO';
 }
-
